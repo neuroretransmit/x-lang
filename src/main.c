fcd4b9fef87b362dd2820122dd466b83a1ff5501
John Holly <typ3def@gmail.com>
2016-11-09 04:31:46 -0700

    Added ASM and output binary argp switches.

--- src/main.c
+++ src/main.c
@@ -24,21 +24,12 @@ static const char* doc = "x-lang -- Reference compiler for x-lang.\n";
 
 struct arguments {
 	char* args[2]; /* ARG1 and ARG2 */
-	char* ir;
-	int ast;
+	bool ir;
+	bool ast;
 	char* bitcode;
+	char* _asm;
 	char* outfile;
 };
-/*
-static void dump_binary(char* fname)
-{
-	char* llc = "llc -o tmp/tmp.s tmp/tmp.bc";
-	char* cc = "cc -o ";
-	strcat(cc, fname);
-	strcat(cc, " tmp/tmp.s");
-	system(llc);
-	system(cc);
-}*/
 
 static void dump_bitcode(char* fname, char* dest)
 {
@@ -63,6 +54,24 @@ static void dump_bitcode(char* fname, char* dest)
 	destroy_ast_node(ast);
 }
 
+static void dump_asm(char* fname, char* dest)
+{
+	dump_bitcode(fname, "/tmp/.bc-tmp");
+	char* llc = malloc(100);
+	sprintf(llc, "llc /tmp/.bc-tmp -o %s", dest);
+	system(llc);
+	destroy(llc);
+}
+
+static void dump_binary(char* fname, char* dest)
+{
+	dump_asm(fname, "/tmp/.asm-tmp.s");
+	char* cc = malloc(100);
+	sprintf(cc, "cc /tmp/.asm-tmp.s -o %s", dest);
+	system(cc);
+	destroy(cc);
+}
+
 static void dump_ast(char* fname)
 {
 	ParserContext* parser = init_parser(fname);
@@ -83,12 +92,8 @@ static void dump_ir(char* fname)
 	
 	char *error = NULL;
 	LLVMVerifyModule(context->module, LLVMAbortProcessAction, &error);
-	LLVMDisposeMessage(error);
-	
+	LLVMDisposeMessage(error);	
 	LLVMDumpModule(context->module);
-
-	LLVMRunFunctionAsMain(context->engine, context->main_func, 0, NULL, NULL);
-	//dump_binary(context->module, "bin/x-lang.bc");
 	
 	destroy_codegen(context);
 	destroy_parser(parser);
@@ -104,22 +109,22 @@ static int parse_opt(int key, char* arg, struct argp_state* state)
 			a->outfile = arg;
 			break;
 		
-		case 667:
-			a->ir = arg;
+		case 'i':
+			a->ir = true;
 			break;
 		
 		case 'b':
 			a->bitcode = arg;
 			break;
 		
-		/*case 669:
-			a->asm = arg;
-			break;*/
-		
 		case 'a':
 			a->ast = true;
+			break;	
+		
+		case 'S':
+			a->_asm = arg;
 			break;
-			
+		
 		case ARGP_KEY_ARG:
 			a->args[state->arg_num] = arg;
 			break;
@@ -146,12 +151,12 @@ int main(int argc, char** argv)
 		{ "output", 'o', "OUTFILE", 0, "Output binary to file.", 0 }, // FIXME
 		{ "ir", 'i', 0, 0, "Dump intermediate representation.", 0 },
 		{ "ast", 'a', 0, 0, "Dump abstract syntax tree.", 0 },
-		{ "asm", 0, "OUTFILE", 669, "Dump the native assembly code.", 0 },
+		{ "asm", 'S', "OUTFILE", 0, "Dump the native assembly code.", 0 },
 		{ "bitcode", 'b', "OUTFILE", 0, "Dump the LLVM bitcode to a file.", 0 },
 		{ 0 }
 	};
 
-	struct arguments arguments;
+	struct arguments arguments = { { 0, 0 }, false, false, NULL, NULL, NULL };
 	struct argp argp = { options, parse_opt, args_doc, doc, NULL, NULL, NULL };
 
 	if (argp_parse(&argp, argc, argv, 0, 0, &arguments) == 0) {
@@ -167,11 +172,14 @@ int main(int argc, char** argv)
 					dump_ir(arguments.args[0]);
 				}
 				
-				if (arguments.bitcode) {
+				if (arguments.bitcode)
 					dump_bitcode(arguments.args[0], arguments.bitcode);
-				}
 				
-				//dump_binary(fname);
+				if (arguments._asm)
+					dump_asm(arguments.args[0], arguments._asm);
+				
+				if (arguments.outfile)
+					dump_binary(arguments.args[0], arguments.outfile);
 			}
 			//prev = fname;
 		}
